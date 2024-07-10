84fa6ccbd8624a85930ede83e618fd1db73b80b6
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-07-10 10:32:50 +0200

    adding conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ function App() {
       id: 1,
       image:
         "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
+      hoverImage:
+        "https://giphy.com/gifs/the-simpsons-scared-homer-simpson-jUwpNzg9IcyrK",
       firstName: "Homer",
       lastName: "Simpson",
     },
     {
       id: 2,
       image: "https://upload.wikimedia.org/wikipedia/en/0/0b/Marge_Simpson.png",
+      hoverImage:
+        "https://giphy.com/gifs/the-simpsons-treehouse-of-horror-ess3aimXqHfyw",
       firstName: "Marge",
       lastName: "Simpson",
     },
@@ -20,12 +24,14 @@ function App() {
       id: 3,
       image:
         "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png",
+      hoverImage: "https://giphy.com/gifs/the-simpsons-snow-cold-EzhYp5ZQXomLm",
       firstName: "Bart",
       lastName: "Simpson",
     },
     {
       id: 4,
       image: "https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png",
+      hoverImage: "https://giphy.com/embed/45O9F2aiLOZXy",
       firstName: "Lisa",
       lastName: "Simpson",
     },
@@ -33,6 +39,7 @@ function App() {
       id: 5,
       image:
         "https://upload.wikimedia.org/wikipedia/en/thumb/9/9d/Maggie_Simpson.png/220px-Maggie_Simpson.png",
+      hoverImage: "https://giphy.com/gifs/the-simpsons-cdZPn0rqZYd9K",
       firstName: "Maggie",
       lastName: "Simpson",
     },
--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,8 +1,19 @@
 /* eslint-disable react/prop-types */
-function Avatar({ image, firstName, lastName }) {
+import { useState } from "react";
+function Avatar({ image, firstName, lastName, hoverImage }) {
+  const [hover, setHover] = useState(false);
+  const handleHover = () => {
+    console.log(hover);
+    setHover(!hover);
+    console.log(hover);
+  };
   return (
-    <div className="card">
-      <img className="card__img" src={image} alt={`${firstName} ${lastName}`} />
+    <div className="card" onMouseOver={() => handleHover()}>
+      <img
+        className="card__img"
+        src={hover ? hoverImage : image}
+        alt={`${firstName} ${lastName}`}
+      />
       <p className="card__text">
         {firstName} {lastName}
       </p>
